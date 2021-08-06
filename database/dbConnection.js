feb7eca3634aca5e9683e268d9ff37e1bc2d9b4d
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-06 23:22:43 +0530

    update: created a database and wired that to my project

---
+++ database/dbConnection.js
@@ -0,0 +1,9 @@
+// mongodb + srv://andleebsyed:<password>@travello.scnn9.mongodb.net/test
+const mongoose = require('mongoose')
+function DbConnection() {
+
+  mongoose.connect(process.env.URI , {useNewUrlParser : true , useUnifiedTopology : true, useCreateIndex: true})
+.then(res => console.log("connected to database"))
+.catch(error => console.log("error occured " , error))
+}
+module.exports = {DbConnection}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+require('dotenv').config()
+const { DbConnection } = require('./database/dbConnection')
+const { userRoute } = require('./routes/userRoute/userRoute')
 const app = express()
-
+DbConnection()
 app.get('/', (req, res) => {
     res.json({ status: true, message: "welcome to entry point of social media backend" })
 })
 
-
+app.use('/user', userRoute)
 app.listen(9000, () => console.log("server is up and running"))
 
