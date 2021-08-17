b524d9a889b8aa473a6c868b3ad4df5ea6ebd8e0
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-17 09:08:56 +0530

    update: gave a defalut profile picture to a new user who signs up

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,12 @@ const { User } = require("../models/user-model");
 const SignUp = async (req, res) => {
   try {
     const secret = process.env.SECRET;
-    const { userDetails } = req.body;
+    let { userDetails } = req.body;
+    userDetails = {
+      ...userDetails,
+      avatar:
+        "https://res.cloudinary.com/andleebsyedcloud/image/upload/v1629091591/Image__1796-2017-01-27_kayqbq.jpg",
+    };
     const newUser = new User(userDetails);
     const salt = await bcrypt.genSalt(10);
     newUser.password = await bcrypt.hash(newUser.password, salt);
@@ -12,7 +17,12 @@ const SignUp = async (req, res) => {
     const token = jwt.sign({ userId: SavedUser._id }, secret, {
       expiresIn: "24h",
     });
-    res.json({ status: true, message: "user added successfully", token });
+    res.json({
+      status: true,
+      message: "user added successfully",
+      token,
+      userId: SavedUser._id,
+    });
   } catch (error) {
     if (error.code === 11000) {
       res.json({
@@ -50,6 +60,7 @@ const SignIn = async (req, res) => {
           allowUser: true,
           message: "logged in successfully",
           token,
+          userId: ourUser._id,
         });
       } else {
         res.json({
