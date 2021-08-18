1a2860293394d2de480d990c4cfeacca50acbb14
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-18 09:36:16 +0530

    update: added timestamp to each entry to collection

--- models/posts-model.js
+++ models/posts-model.js
@@ -12,35 +12,40 @@ const CommentSchema = new Schema(
     timestamps: true,
   }
 );
-const PostSchema = Schema({
-  postImage: {
-    type: String,
-  },
-  postText: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  likes: {
-    type: Number,
-  },
-  timestamp: {
-    type: Date,
-    required: true,
-  },
-  likedBy: [
-    {
+const PostSchema = Schema(
+  {
+    postImage: {
+      type: String,
+    },
+    postText: {
+      type: String,
+      required: true,
+    },
+    author: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
-  ],
-  comments: [CommentSchema],
-});
+    likes: {
+      type: Number,
+    },
+    timestamp: {
+      type: Date,
+      required: true,
+    },
+    likedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    comments: [CommentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Post = model("Post", PostSchema);
 
