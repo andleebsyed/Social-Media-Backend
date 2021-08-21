14bf9cbfb441207b487dcfe41d5afba0905f3ee5
Syed Andleeb <andleebsyed75@gmail.com>
2021-08-21 08:59:36 +0530

    update: added some routes to handle multiple things

--- routes/userRoute/userRoute.js
+++ routes/userRoute/userRoute.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const { SignIn, SignUp } = require('../../controllers/users')
-const userRoute = express.Router()
+const express = require("express");
+const {
+  SignIn,
+  SignUp,
+  UserDetails,
+  UpdateUser,
+  GetAllUsers,
+} = require("../../controllers/users");
+const userRoute = express.Router();
+const { verifyToken } = require("../../middlewares/verifyToken");
+userRoute.post("/signin", SignIn);
+userRoute.post("/signup", SignUp);
+userRoute.post("/", verifyToken, UserDetails);
+userRoute.post("/update", verifyToken, UpdateUser);
+userRoute.post("/allusers", verifyToken, GetAllUsers);
 
-userRoute.post('/signin', SignIn)
-userRoute.post('/signup', SignUp)
-
-module.exports = {userRoute}
+module.exports = { userRoute };
